00c7b55532d82912d52679062a8c6bea272d13f7
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-05-03 15:10:12 +0100

    higher order components

--- src/App.js
+++ src/App.js
@@ -1,54 +1,15 @@
 import "./App.css";
-import ClassClick from "./components/ClassClick";
-import EventBind from "./components/EventBind";
-// import Greet from "./components/Greet";
-// import Welcome from "./components/Welcome";
-// import Message from "./components/Message";
-// import Counter from "./components/Counter";
-import FunctionClick from "./components/FunctionClick";
-import NameList from "./components/NameList";
-import ParentComponent from "./components/parentComponent";
-import UserGreeting from "./components/UserGreeting";
-import Stylesheet from "./components/Stylesheet";
-import Inline from "./components/Inline";
-import Form from "./components/Form";
-import LifeCycleA from "./components/LifeCycleA";
-import FragmentDemo from "./components/FragmentDemo";
-import Table from "./components/Table";
-import PureCompo from "./components/PureComponent";
-import MemoComponent from "./components/MemoComponent";
-import RefsDemo from "./components/RefsDemo";
-import FocusInput from "./components/FocusInput";
-import TestRef from "./components/TestRef";
-import RefsTest from "./components/RefsTest";
-import PortalDemo from "./components/PortalDemo";
 import Hero from "./components/Hero";
 import ErrorBoundary from "./ErrorBoundary";
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter/>
-      <Greet name='Diana' heroName='Wonder Woman'/>
-      <Message />
-      <Welcome name='Bruce Wayne' heroName='Batman'/> */}
-      {/* <EventBind/>
-      <ParentComponent/> */}
-      {/* <UserGreeting/>
-      <NameList/>
-      <Stylesheet/>
-      <Inline/> */}
-      {/* <LifeCycleA/> */}
-      {/* <Table/> */}
-      {/* <FragmentDemo/> */}
-      {/* <Form/> */}
-      {/* <ParentComponent/> */}
-      {/* <FocusInput/> */}
-      {/* <RefsTest/>
-      <PortalDemo/> */}
+
       <ErrorBoundary>
         <Hero heroName="Joker" />
       </ErrorBoundary>
+      <Hero heroName='Bruce'/>
     </div>
   );
 }
---
+++ src/ClickCounter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ClickCounter extends Component {
+  render() {
+    return (
+      <div>ClickCounter</div>
+    )
+  }
+}
+
+export default ClickCounter
