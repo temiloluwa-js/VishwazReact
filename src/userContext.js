fd92535a1d951966aa783d38ed5d31f3748da08b
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-05-04 16:50:24 +0100

    hooks beginning

--- src/userContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const UserContext = React.createContext('Code Evolution')
-
-const UserConsumer = UserContext.Consumer
-const UserProvider = UserContext.Provider
-
-export {UserConsumer, UserProvider}
-export default UserContext
